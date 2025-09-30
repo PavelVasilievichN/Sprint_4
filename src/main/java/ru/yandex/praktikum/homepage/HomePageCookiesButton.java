5e3a72c02651cffb471ec88179f34a46bbe9279e
Pavel Novoselov <pavel_informatika@mail.ru>
2025-09-30 13:49:02 +0500

    Made changes to the HomePageCookiesButton.java

--- src/main/java/ru/yandex/praktikum/homepage/HomePageCookiesButton.java
+++ src/main/java/ru/yandex/praktikum/homepage/HomePageCookiesButton.java
@@ -1,25 +1,22 @@
+
 package ru.yandex.praktikum.homepage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static ru.yandex.praktikum.util.EvnConfig.EXPLICITY_TIMEOUT;
+import ru.yandex.praktikum.util.EvnConfig;
 
 public class HomePageCookiesButton {
     private final WebDriver driver;
+    private final EvnConfig config = new EvnConfig();
     //локатор для кнопки принятия кук
     private final By acceptingCookies = By.xpath(".//button[@id='rcc-confirm-button']");
 
     public HomePageCookiesButton(WebDriver driver) {
         this.driver = driver;
     }
-
     //Метод принять куки нажатием на кнопку
-    public void acceptCookiesButton(){
-        new WebDriverWait(driver, EXPLICITY_TIMEOUT)
-                .until(ExpectedConditions.elementToBeClickable(acceptingCookies));
+    public void clickAcceptCookiesButton(){
+        config.waitElementToBeClickable(driver, acceptingCookies);
         driver.findElement(acceptingCookies).click();
     }
 }
