0f4ca22280d3b54c95455212156450d235d65fa8
Pavel Novoselov <pavel_informatika@mail.ru>
2025-09-30 13:38:30 +0500

    Added explicit delay methods

--- src/main/java/ru/yandex/praktikum/util/EvnConfig.java
+++ src/main/java/ru/yandex/praktikum/util/EvnConfig.java
@@ -1,7 +1,24 @@
 package ru.yandex.praktikum.util;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class EvnConfig {
     public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
     public static final String SCROLL_ELEMENT = "arguments[0].scrollIntoView();";
-    public static final int EXPLICITY_TIMEOUT = 10;
+    public static final int EXPLICIT_TIMEOUT = 5;
+
+    public void waitVisibilityOfElementLocated(WebDriver driver, By elementLocator){
+        new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_TIMEOUT))
+                .until(ExpectedConditions.visibilityOfElementLocated(elementLocator));
+    }
+
+    public void waitElementToBeClickable(WebDriver driver, By elementLocator){
+        new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_TIMEOUT))
+                .until(ExpectedConditions.elementToBeClickable(elementLocator));
+    }
 }
