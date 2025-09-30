d1f02c919a27c883d4c9e465b3db1ac8cbe324e0
Pavel Novoselov <pavel_informatika@mail.ru>
2025-09-30 14:31:10 +0500

    Added DropDownListItemPossibleOrderScooterTodayTest.java

---
+++ src/test/java/ru/yandex/praktikum/tests/tools/dropdownlist/DropDownListItemPossibleOrderScooterTodayTest.java
@@ -0,0 +1,55 @@
+package ru.yandex.praktikum.tests.tools.dropdownlist;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import ru.yandex.praktikum.homepage.HomePageYandexScooter;
+import ru.yandex.praktikum.homepage.HomePageCookiesButton;
+
+import static org.junit.Assert.assertEquals;
+import static ru.yandex.praktikum.tests.tools.ToolBrowsers.getBrowser;
+
+@RunWith(Parameterized.class)
+public class DropDownListItemPossibleOrderScooterTodayTest {
+    private WebDriver driver;
+    private final String question;
+    private final String answer;
+    private final boolean expectedResult;
+
+    public DropDownListItemPossibleOrderScooterTodayTest(String question, String answer, boolean expectedResult) {
+        this.question = question;
+        this.answer = answer;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void setUp() {
+        driver = getBrowser("chrome");
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData(){
+        return new Object[][]{ {"Можно ли заказать самокат прямо на сегодня?",
+                "Только начиная с завтрашнего дня. Но скоро станем расторопнее.", true}
+        };
+    }
+
+    @Test
+    public void shouldAnswerCorrespondQuestionPossibleOrderScooterToday(){
+        var scooter = new HomePageYandexScooter(driver);
+        scooter.openHomePage(driver);
+        var cookiesButton = new HomePageCookiesButton(driver);
+        cookiesButton.clickAcceptCookiesButton();
+        scooter.scrollToTheElement(scooter.getDropDownListItemHowMuchCostAndHowPay());
+        boolean actualText = scooter.checkDropDownListItemPossibleOrderScooterToday(question, answer);
+        assertEquals("Ответ несоответствует вопросу", expectedResult, actualText);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+}
