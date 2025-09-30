c6be830cbf13f42a805b2d31fdcbab7fc3a2ebcc
Pavel Novoselov <pavel_informatika@mail.ru>
2025-09-30 14:14:22 +0500

    Added chain command methods to increase the browser window size

--- src/test/java/ru/yandex/praktikum/tests/tools/ToolBrowsers.java
+++ src/test/java/ru/yandex/praktikum/tests/tools/ToolBrowsers.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ToolBrowsers {
+
     public static WebDriver getBrowser(String browserName) {
         WebDriver driver = null;
         switch (browserName) {
             case "firefox":
                 driver = new FirefoxDriver();
+                driver.manage().window().maximize();
                 break;
             case "chrome":
                 driver = new ChromeDriver();
+                driver.manage().window().maximize();
                 break;
         }
         return driver;
