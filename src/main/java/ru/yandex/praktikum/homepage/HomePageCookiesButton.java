f9f3e816e632bd9ea3015660e54192d852d243b4
Pavel Novoselov <pavel_informatika@mail.ru>
2025-10-02 13:34:15 +0500

    Removed unnecessary indents

--- src/main/java/ru/yandex/praktikum/homepage/HomePageCookiesButton.java
+++ src/main/java/ru/yandex/praktikum/homepage/HomePageCookiesButton.java
@@ -1,22 +1,25 @@
-
 package ru.yandex.praktikum.homepage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import ru.yandex.praktikum.util.EvnConfig;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static ru.yandex.praktikum.util.EvnConfig.EXPLICIT_TIMEOUT;
 
 public class HomePageCookiesButton {
     private final WebDriver driver;
-    private final EvnConfig config = new EvnConfig();
     //локатор для кнопки принятия кук
     private final By acceptingCookies = By.xpath(".//button[@id='rcc-confirm-button']");
 
     public HomePageCookiesButton(WebDriver driver) {
         this.driver = driver;
     }
+
     //Метод принять куки нажатием на кнопку
     public void clickAcceptCookiesButton(){
-        config.waitElementToBeClickable(driver, acceptingCookies);
+        new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_TIMEOUT))
+                .until(ExpectedConditions.elementToBeClickable(acceptingCookies));
         driver.findElement(acceptingCookies).click();
     }
 }
